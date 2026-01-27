b977ed8cf73bebfb4ffec80b89fb3cf35806218a
AnvitaAnanya <anvitaananya93@gmail.com>
2026-01-27 10:59:15 +0530

    this is the final commit

--- tourizio/src/environments/firebase.config.ts
+++ tourizio/src/environments/firebase.config.ts
@@ -1,9 +1,9 @@
 // Firebase configuration
 // Replace with your Firebase project credentials from Firebase Console
 export const firebaseConfig = {
-  apiKey: "AIzaSyDXQy2L0BqJE7l21SKxQVgFlv-GXdMHo1A",
-  authDomain: "tourizio-6fe0f.firebaseapp.com",
-  projectId: "tourizio-6fe0f",
+  apiKey: (import.meta as any).env.NG_APP_FIREBASE_API_KEY,
+  authDomain: (import.meta as any).env.NG_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: (import.meta as any).env.NG_APP_FIREBASE_PROJECT_ID,
   storageBucket: "tourizio-6fe0f.firebasestorage.app",
   messagingSenderId: "563599121560",
   appId: "1:563599121560:web:4a689a6971c371cd4d84ed"
